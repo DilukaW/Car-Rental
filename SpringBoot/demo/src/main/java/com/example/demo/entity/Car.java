225fac0b1b644957f35246442210f6714294d0ea
Diluka <dt.wj85@gmail.com>
2024-12-09 11:27:44 +0530

    firebase added to spring boot

---
+++ SpringBoot/demo/src/main/java/com/example/demo/entity/Car.java
@@ -0,0 +1,80 @@
+package com.example.demo.model;
+
+
+public class Car {
+    private String id;
+    private String name;
+    private String type;
+    private String doors;
+    private String price;
+    private String features;
+    private String image;
+    private String seats;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDoors() {
+        return doors;
+    }
+
+    public void setDoors(String doors) {
+        this.doors = doors;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(String features) {
+        this.features = features;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getSeats() {
+        return seats;
+    }
+
+    public void setSeats(String seats) {
+        this.seats = seats;
+    }
+
+    // Getters and Setters
+
+}
